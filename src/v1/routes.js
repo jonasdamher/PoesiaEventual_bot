7e29d516e1e700e52bfee79f451d2c4c39d6ddd9
Jonás Damián <jonas.damher@gmail.com>
2021-04-16 11:37:39 +0100

    UPDATE: add routes commands

--- src/v1/routes.js
+++ src/v1/routes.js
@@ -1,10 +1,18 @@
 'use strict';
 
 const bot = require('./config/bot')
-const moduleGeneric = require('./functions/generic')
-const moduleAuthor = require('./functions/author')
 
-bot.onText(/^\/start$/, moduleGeneric.start);
-bot.onText(/^\/author(.+[a-zA-Z])/, moduleAuthor.author)
+const generic = require('./functions/generic')
+const author = require('./functions/author')
+const poem = require('./functions/poem')
+
+bot.onText(/^\/iniciar|\/start$/, generic.start);
+bot.onText(/^\/ayuda$/, generic.help);
+
+bot.onText(/^\/autor([\s]{1,}.+[a-zA-Z-0-9])/, author.author)
+bot.onText(/^\/descubrir_autor$/, author.discoverAuthor)
+
+bot.onText(/^\/poema([\s]{1,}.+[a-zA-Z-0-9])/, poem.poem)
+bot.onText(/^\/descubrir_poema$/, poem.discoverPoem)
 
 module.exports= bot
