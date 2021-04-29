c70f7733e11f713f95d5a3330b76d10d63ebe8fc
Jonás Damián <jonas.damher@gmail.com>
2021-04-29 15:29:47 +0100

    add express

--- index.js
+++ index.js
@@ -6,5 +6,9 @@ dotenv.config()
 // for warning message deprecated of api telegram.
 process.env.NTBA_FIX_319 = 1;
 
+const express = require('express')
+const app = express()
+app.listen(process.env.PORT)
+
 const routes = './src/bot/' + process.env.VERSION + '/routes'
 require(routes)
