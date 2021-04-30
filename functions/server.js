51310fcd502b6f8536db356d08f334e3ef32a1b9
Jonás Damián <jonas.damher@gmail.com>
2021-04-30 19:44:16 +0100

    UPDATE: change scripts, remove routes

--- functions/server.js
+++ functions/server.js
@@ -6,6 +6,17 @@ dotenv.config()
 // for warning message deprecated of api telegram.
 process.env.NTBA_FIX_319 = 1;
 
-const routes = './routes'
- 
-require(routes)
+const bot = require('./config/bot')
+const generic = require('./controller/generic')
+const author = require('./controller/author')
+const poem = require('./controller/poem')
+
+bot.onText(/^\/iniciar|\/start$/, generic.start);
+bot.onText(/^\/ayuda$/, generic.help);
+
+bot.onText(/^\/autor([\s]{1,}.+[a-zA-Z-0-9])/, author.author)
+bot.onText(/^\/descubrir_autor$/, author.discoverAuthor)
+
+bot.onText(/^\/poema([\s]{1,}.+[a-zA-Z-0-9])/, poem.poem)
+bot.onText(/^\/poemas([\s]{1,}.+[a-zA-Z-0-9])/, poem.poems)
+bot.onText(/^\/descubrir_poema$/, poem.discoverPoem)
