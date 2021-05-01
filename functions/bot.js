8a289676f7545c9e350e87324a0d758499e36a82
Jonás Damián <jonas.damher@gmail.com>
2021-05-01 19:17:54 +0100

    UPDATE: comment actions author and poem

--- functions/bot.js
+++ functions/bot.js
@@ -1,18 +1,18 @@
 'use strict';
 
 const generic = require('./actions/generic')
-const author = require('./actions/author')
-const poem = require('./actions/poem')
+// const author = require('./actions/author')
+// const poem = require('./actions/poem')
 
 const bot = require('./config/bot')
 
 bot.command(/^\/iniciar|\/start$/, msg => generic.start(msg))
 bot.command(/^\/ayuda$/, msg => generic.help(msg))
 
-bot.command(/^\/descubrir_autor$/, msg => author.discover(msg))
+// bot.command(/^\/descubrir_autor$/, msg => author.discover(msg))
 // bot.on(/^\/autor([\s]{1,}.+[a-zA-Z-0-9])/, msg => author.get(msg))
 
-bot.command(/^\/descubrir_poema$/, msg => poem.discover(msg))
+// bot.command(/^\/descubrir_poema$/, msg => poem.discover(msg))
 // bot.on(/^\/poema([\s]{1,}.+[a-zA-Z-0-9])/, msg => poem.poem(msg))
 // bot.on(/^\/poemas([\s]{1,}.+[a-zA-Z-0-9])/, poem.poems)
 
