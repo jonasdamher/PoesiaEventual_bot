2581e9f7cc77d968fb958cb22a3471a59694f705
Jonás Damián <jonas.damher@gmail.com>
2021-05-01 19:16:01 +0100

    UPDATE: change bot.on for bot.command

--- functions/bot.js
+++ functions/bot.js
@@ -6,13 +6,13 @@ const poem = require('./actions/poem')
 
 const bot = require('./config/bot')
 
-bot.on(/^\/iniciar|\/start$/, msg => generic.start(msg))
-bot.on(/^\/ayuda$/, msg => generic.help(msg))
+bot.command(/^\/iniciar|\/start$/, msg => generic.start(msg))
+bot.command(/^\/ayuda$/, msg => generic.help(msg))
 
-bot.on(/^\/descubrir_autor$/, msg => author.discover(msg))
+bot.command(/^\/descubrir_autor$/, msg => author.discover(msg))
 // bot.on(/^\/autor([\s]{1,}.+[a-zA-Z-0-9])/, msg => author.get(msg))
 
-bot.on(/^\/descubrir_poema$/, msg => poem.discover(msg))
+bot.command(/^\/descubrir_poema$/, msg => poem.discover(msg))
 // bot.on(/^\/poema([\s]{1,}.+[a-zA-Z-0-9])/, msg => poem.poem(msg))
 // bot.on(/^\/poemas([\s]{1,}.+[a-zA-Z-0-9])/, poem.poems)
 
