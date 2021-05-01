f012268dfb692974ee9b2c7aa3c378d5a1c1b385
Jonás Damián <jonas.damher@gmail.com>
2021-05-01 19:30:09 +0100

    UPDATE: add method discover of poem and author

--- functions/bot.js
+++ functions/bot.js
@@ -1,19 +1,18 @@
 'use strict';
 
 const generic = require('./actions/generic')
-// const author = require('./actions/author')
-// const poem = require('./actions/poem')
+const author = require('./actions/author')
+const poem = require('./actions/poem')
 
 const bot = require('./config/bot')
 
 bot.hears(/^\/iniciar|\/start$/, ctx => generic.start(ctx))
 bot.hears(/^\/ayuda$/, ctx => generic.help(ctx))
 
-
-// bot.command(/^\/descubrir_autor$/, msg => author.discover(msg))
+bot.hears(/^\/descubrir_autor$/, msg => author.discover(msg))
 // bot.on(/^\/autor([\s]{1,}.+[a-zA-Z-0-9])/, msg => author.get(msg))
 
-// bot.command(/^\/descubrir_poema$/, msg => poem.discover(msg))
+bot.hears(/^\/descubrir_poema$/, msg => poem.discover(msg))
 // bot.on(/^\/poema([\s]{1,}.+[a-zA-Z-0-9])/, msg => poem.poem(msg))
 // bot.on(/^\/poemas([\s]{1,}.+[a-zA-Z-0-9])/, poem.poems)
 
