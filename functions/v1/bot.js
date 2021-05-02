83d3b4f903955eece5824f6b0f272ee3efaf2120
Jonás Damián <jonas.damher@gmail.com>
2021-05-02 01:21:52 +0100

    FIX: change type endpoint autor

--- functions/v1/bot.js
+++ functions/v1/bot.js
@@ -10,11 +10,11 @@ bot.hears(/^\/iniciar|\/start$/, ctx => generic.start(ctx))
 bot.hears(/^\/ayuda$/, ctx => generic.help(ctx))
 
 bot.hears(/^\/descubrir_autor$/, msg => author.discover(msg))
-bot.on(/^\/autor([\s]{1,}.+[a-zA-Z-0-9])/, msg => author.get(msg))
+bot.hears(/^\/autor([\s]{1,}.+[a-zA-Z-0-9])/, msg => author.get(msg))
 
 bot.hears(/^\/descubrir_poema$/, msg => poem.discover(msg))
-// bot.on(/^\/poema([\s]{1,}.+[a-zA-Z-0-9])/, msg => poem.get(msg))
-// bot.on(/^\/poemas([\s]{1,}.+[a-zA-Z-0-9])/, poem.poems)
+// bot.hears(/^\/poema([\s]{1,}.+[a-zA-Z-0-9])/, msg => poem.get(msg))
+// bot.hears(/^\/poemas([\s]{1,}.+[a-zA-Z-0-9])/, poem.poems)
 
 exports.handler = async event => {
     try {
