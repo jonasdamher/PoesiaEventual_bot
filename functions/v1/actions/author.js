8b6d96d36d6c4ed10e2a23b3dcb323f1f9e7b903
Jonás Damián <jonas.damher@gmail.com>
2021-05-02 17:41:12 +0100

    UPDATE: testing example callback_query telegraf, console.log example

--- functions/v1/actions/author.js
+++ functions/v1/actions/author.js
@@ -92,7 +92,7 @@ async function authorSearch(msg, authorName) {
             let { message, list } = createAuthorsList(authorName, data)
 
             bot.on('callback_query', (ctx) => {
-                console.log('callbackQuery ye ' + ctx.data)
+                console.log('callbackQuery ye ',ctx)
                 // return get(msg)
                 ctx.answerCbQuery()
             })
