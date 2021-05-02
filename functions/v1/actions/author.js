24348a380e48391d346f0c6ac3f22c2d6c975b22
Jonás Damián <jonas.damher@gmail.com>
2021-05-02 01:59:11 +0100

    FIX: get match parameter

--- functions/v1/actions/author.js
+++ functions/v1/actions/author.js
@@ -22,110 +22,109 @@ async function discover(msg) {
     })
 }
 
-async function get(msg, match) {
+async function get(msg) {
 
-    const data = match[1].trim()
-    msg.reply(data)
+    const data = msg.match[1].trim()
 
-    // if (helper.isId(data)) {
-    //     return sendAuthorById(msg, data)
-    // } else {
-    //     return authorSearch(msg, data)
-    // }
+    if (helper.isId(data)) {
+        return sendAuthorById(msg, data)
+    } else {
+        return authorSearch(msg, data)
+    }
 }
 
-// async function sendAuthorById(msg, id) {
+async function sendAuthorById(msg, id) {
 
-//     return axios.get('author/get/' + id).then(res => {
-//         return searchAuthorWiki(msg, res.data.name)
-//     }).catch(err => {
-//         msg.reply('Disculpa, hubo un error al tratar de encontrar una referencia sobre *' + authorName + '*.')
-//     })
-// }
+    return axios.get('author/get/' + id).then(res => {
+        return searchAuthorWiki(msg, res.data.name)
+    }).catch(err => {
+        msg.reply('Disculpa, hubo un error al tratar de encontrar una referencia sobre *' + authorName + '*.')
+    })
+}
 
-// function createAuthorsList(authorName, data) {
+function createAuthorsList(authorName, data) {
 
-//     // add authors
-//     let list = data.authors.map(author => {
-//         return [{
-//             id: author._id,
-//             title: author.name,
-//             type: 'article',
-//             input_message_content: {
-//                 message_text: author.name,
-//                 parse_mode: 'HTML'
-//             }
-//         }]
-//     })
+    // add authors
+    let list = data.authors.map(author => {
+        return [{
+            id: author._id,
+            title: author.name,
+            type: 'article',
+            input_message_content: {
+                message_text: author.name,
+                parse_mode: 'HTML'
+            }
+        }]
+    })
 
-//     // add pagination
-//     let filterAuthorName = helper.filterTextForPagination(authorName)
+    // add pagination
+    let filterAuthorName = helper.filterTextForPagination(authorName)
 
-//     let currentPage = data.pagination.page
+    let currentPage = data.pagination.page
 
-//     if (currentPage < data.pagination.lastPage) {
-//         ++currentPage
+    if (currentPage < data.pagination.lastPage) {
+        ++currentPage
 
-//         let url = filterAuthorName + '?perpage=' + data.pagination.perPage + '&page=' + currentPage
-//         let messagePagination = 'Mas autores ' + data.pagination.page + '/' + data.pagination.lastPage
+        let url = filterAuthorName + '?perpage=' + data.pagination.perPage + '&page=' + currentPage
+        let messagePagination = 'Mas autores ' + data.pagination.page + '/' + data.pagination.lastPage
 
-//         list.push([{
-//             id: url,
-//             title: messagePagination,
-//             type: 'article',
-//             input_message_content: {
-//                 message_text: messagePagination,
-//                 parse_mode: 'HTML'
-//             }
-//         }])
-//     }
+        list.push([{
+            id: url,
+            title: messagePagination,
+            type: 'article',
+            input_message_content: {
+                message_text: messagePagination,
+                parse_mode: 'HTML'
+            }
+        }])
+    }
 
-//     let message = ''
-//     if (!authorName.includes('?perpage=')) {
-//         message = 'He encontrado ' + data.pagination.total + ' coincidencias relacionadas con *' + filterAuthorName + '*,\nquizás estas buscando:'
-//     } else {
-//         message = 'Página ' + data.pagination.page + ':'
-//     }
-//     return { message, list }
-// }
+    let message = ''
+    if (!authorName.includes('?perpage=')) {
+        message = 'He encontrado ' + data.pagination.total + ' coincidencias relacionadas con *' + filterAuthorName + '*,\nquizás estas buscando:'
+    } else {
+        message = 'Página ' + data.pagination.page + ':'
+    }
+    return { message, list }
+}
 
-// async function authorSearch(msg, authorName) {
+async function authorSearch(msg, authorName) {
 
-//     let search = helper.addParams(authorName)
+    let search = helper.addParams(authorName)
 
-//     return axios.get('author/search/' + search).then(res => {
+    return axios.get('author/search/' + search).then(res => {
 
-//         let data = res.data
+        let data = res.data
 
-//         if (data.authors.length == 1 &&
-//             data.pagination.page == 1 &&
-//             data.pagination.lastPage == 1
-//         ) {
+        if (data.authors.length == 1 &&
+            data.pagination.page == 1 &&
+            data.pagination.lastPage == 1
+        ) {
 
-//             let authorNameOne = data.authors[0].name
-//             return searchAuthorWiki(msg, authorNameOne)
+            let authorNameOne = data.authors[0].name
+            return searchAuthorWiki(msg, authorNameOne)
 
-//         } else if (data.authors.length > 0) {
+        } else if (data.authors.length > 0) {
 
-//             let { message, list } = createAuthorsList(authorName, data)
+            let { message, list } = createAuthorsList(authorName, data)
 
-//             msg.reply(message)
-//             msg.answerInlineQuery(list)
+            msg.reply(message)
+            msg.answerInlineQuery(list)
 
-//             return bot.removeListener('callback_query').on('callback_query', res => {
-//                 let currentData = ['', res.data]
-//                 return author(msg, currentData)
-//             })
+            return bot.removeListener('callback_query').on('callback_query', res => {
+                msg.match[1] = ['', res.data.trim()]
+                return author(msg)
+            })
 
-//         } else if (!data.authors.length) {
+        } else if (!data.authors.length) {
 
-//             msg.reply('Disculpa, no se ha podido encontrar una referencia sobre *' + authorName + '*.')
-//         }
+            msg.reply('Disculpa, no se ha podido encontrar una referencia sobre *' + authorName + '*.')
+        }
 
-//     }).catch(err => {
-//         msg.reply('Disculpa, hubo un error al tratar de encontrar una referencia sobre *' + authorName + '*.')
-//     })
-// }
+    }).catch(err => {
+        msg.reply('Disculpa, hubo un error al tratar de encontrar una referencia sobre *' + authorName + '*.')
+    })
+}
 
 async function searchAuthorWiki(msg, authorName, i = 0) {
 
