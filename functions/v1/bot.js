f0debfffcc54347e8e03ab0736dec727e8cf5436
Jonás Damián <jonas.damher@gmail.com>
2021-05-09 21:40:59 +0100

    UPDATE: comment endpoints poems

--- functions/v1/bot.js
+++ functions/v1/bot.js
@@ -2,10 +2,11 @@
 
 const generic = require('./actions/generic')
 const author = require('./actions/author')
-const poem = require('./actions/poem')
+// const poem = require('./actions/poem')
 const bot = require('./config/bot')
 
 bot.on('callback_query', ctx => {
+    console.log('okoloco')
     // return ctx.reply('claro que sí')
     //ctx.answerCbQuery()
 })
@@ -16,8 +17,8 @@ bot.hears(/^\/ayuda$/, msg => generic.help(msg))
 bot.hears(/^\/descubrir_autor$/, msg => author.discover(msg))
 bot.hears(/^\/autor([\s]{1,}.+[a-zA-Z-0-9])/, msg => author.get(msg))
 
-bot.hears(/^\/descubrir_poema$/, msg => poem.discover(msg))
-bot.hears(/^\/poema([\s]{1,}.+[a-zA-Z-0-9])/, msg => poem.get(msg))
+// bot.hears(/^\/descubrir_poema$/, msg => poem.discover(msg))
+// bot.hears(/^\/poema([\s]{1,}.+[a-zA-Z-0-9])/, msg => poem.get(msg))
 // bot.hears(/^\/poemas([\s]{1,}.+[a-zA-Z-0-9])/, msg => poem.get_all_poems_of_author(msg))
 
 exports.handler = async event => {
