b73e179584baed89cfde1d7ed900cb71e352a11b
Jonás Damián <jonas.damher@gmail.com>
2021-05-10 11:49:42 +0100

    UPDATE: add comments

--- functions/v1/bot.js
+++ functions/v1/bot.js
@@ -5,8 +5,14 @@ const author = require('./actions/author')
 const poem = require('./actions/poem')
 const bot = require('./config/bot')
 
+// Endpoints
+// Endpoint for buttons
 bot.on('callback_query', msg => {
+    
+    // 0 = method
+    // 1 = value
     let data = msg.update.callback_query.data.split(/:(.*)/)
+
     msg.match = ['', data[1]]
     if (author[data[0]]) {
         return author[data[0]](msg)
@@ -17,12 +23,15 @@ bot.on('callback_query', msg => {
     }
 })
 
+// Endpoints generics
 bot.hears(/^\/iniciar|\/start$/, msg => generic.start(msg))
 bot.hears(/^\/ayuda$/, msg => generic.help(msg))
 
+// Endpoints authors
 bot.hears(/^\/descubrir_autor$/, msg => author.discover(msg))
 bot.hears(/^\/autor([\s]{1,}.+[a-zA-Z-0-9])/, msg => author.get_author(msg))
 
+// Endpoints poems
 bot.hears(/^\/descubrir_poema$/, msg => poem.discover(msg))
 bot.hears(/^\/poema([\s]{1,}.+[a-zA-Z-0-9])/, msg => poem.get_poem(msg))
 bot.hears(/^\/poemas([\s]{1,}.+[a-zA-Z-0-9])/, msg => poem.get_poems_author(msg))
